1757eb78c6d66802123b98bbe635d3932c4f13d5
RiJul <rijulagarwal0909@gmail.com>
2021-02-04 17:34:29 +0530

    env variable

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ import google_search
 import history
 
 # load_dotenv()
-TOKEN = os.environ('DISCORD_TOKEN')
-GUILD = os.environ('DISCORD_GUILD')
+TOKEN = os.environ.get('DISCORD_TOKEN')
+GUILD = os.environ.get('DISCORD_GUILD')
 
 # defining local variable
 GREETINGS=["hi","hey"]
